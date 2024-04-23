0f679cfcf8fa64f73d32d11958fcad4cd50e4de0
plgkiet <giakiet30112003pgbd@gmail.com>
2024-04-23 15:02:24 +0700

    Update Property.java

---
+++ Property.java
@@ -0,0 +1,40 @@
+public class Property {
+    private String propertyID;
+    private String description;
+    private String propertyOwnerID;
+    
+    public Property(String propertyID, String description, String propertyOwnerID) {
+        this.propertyID = propertyID;
+        this.description = description;
+        this.propertyOwnerID = propertyOwnerID;
+    }
+
+    public String getPropertyID() {
+        return propertyID;
+    }
+
+    public void setPropertyID(String propertyID) {
+        this.propertyID = propertyID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPropertyOwnerID() {
+        return propertyOwnerID;
+    }
+
+    public void setPropertyOwnerID(String propertyOwnerID) {
+        this.propertyOwnerID = propertyOwnerID;
+    }
+
+    
+
+    
+
+}
