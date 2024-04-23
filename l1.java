cdaaa0bc633272871dc78c828855c479dab3e90b
plgkiet <giakiet30112003pgbd@gmail.com>
2024-04-23 13:39:44 +0700

    Update l1.java

--- l1.java
+++
@@ -1,8 +0,0 @@
-
-public class l1 {
-    static StringBuilder str = new StringBuilder();
-    public static void main(String[] args) {
-        str.append("Hello");   
-        System.out.println(str);
-    }    
-}
